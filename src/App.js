d9e9ac1844370284cfa91a1a6bb8df2a0335f8ff
codezen28 <vaibhavgupta43911@gmail.com>
2024-03-23 21:47:33 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+import styled, { ThemeProvider } from "styled-components";
 import "./App.css";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar/Navbar";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Skills from "./components/Skills/Skills";
+import EducationSection from "./components/EducationSection/EducationSection";
+import { BrowserRouter } from "react-router-dom";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <Navbar />
+
+        <Body>
+          <HeroSection />
+          <Wrapper />
+          <Skills />
+          <EducationSection />
+        </Body>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
